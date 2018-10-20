a7a991fa5113e1add96dd9ab02bd38d671bdad7d
Claire Leu-Rodriguez <cleu@cs.washington.edu>
2018-10-20 00:29:09 -0700

    initial commit.

---
+++ server.js
@@ -0,0 +1,36 @@
+console.log("test");
+const express = require('express');
+const bodyParser= require('body-parser');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://user:userpwd1@ds139428.mlab.com:39428/messages';
+
+var db;
+MongoClient.connect(url, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('messages') 
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.get('/', (req, res) => {
+  db.collection('messages').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+app.post('/messages', (req, res) => {
+  db.collection('messages').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('Saved to database!')
+    res.redirect('/')
+  })
+})
+
+
